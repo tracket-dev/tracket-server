34874f21c29efae30bb38d266d5ecf4304e8e3c4
Severiano Badajoz <severiano.badajoz@students.makeschool.com>
2020-03-19 12:24:16 -0700

    add up deployment

--- index.ts
+++ index.ts
@@ -37,4 +37,6 @@ const server = new GraphQLServer({
   schema,
   context: { prisma }
 });
-server.start(() => console.log("Server is running on http://localhost:4000"));
+server.start(({ port }) =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
